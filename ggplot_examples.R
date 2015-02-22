a66095089189a0f77728be9858ff50aede9d6513
Dan <dan.magee.44@gmail.com>
2015-02-22 14:38:26 -0700

    Add in the d2 stuff and plot by color, price, carat

--- ggplot_examples.R
+++ ggplot_examples.R
@@ -20,3 +20,16 @@ summary(myplot)
 # because we're mapping, not setting
 myplot + geom_point(aes(shape=Species), size = 3)
 
+# want the first element of dim(diamonds) is the # of rows
+# sample from 1 to the number of rows 1,000 times
+# those are the rows we want
+# way to randomly pull from 1 to the length of our dataset 1000 times
+# store as d2
+d2 <- diamonds[sample(1:dim(diamonds)[1], 1000), ]
+
+# look at the data set this way... decide to plot by carat by price
+# sort the color by color after seeing this
+head(d2)
+d2plot <- ggplot(d2, aes(x=price, y=carat, color=color))
+
+d2plot + geom_point()
